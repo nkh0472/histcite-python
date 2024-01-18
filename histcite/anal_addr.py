cf6d3158aa850e00879ebf931689b1a4287623be
nkh0472 <67589323+nkh0472@users.noreply.github.com>
2024-01-18 20:04:33 +0800

    Update anal_addr.py

--- histcite/anal_addr.py
+++ histcite/anal_addr.py
@@ -1,14 +1,1 @@
-"""This module is used to analyse the Address info.
 
-Only support WOS data:
-- I2
-- CO
-"""
-
-def SumAddrInfo(df, column='I2'):
-    def merge_lists(row):
-        # Merge a list without duplicates
-        return set([sublist.strip() for sublist in row])
-
-    result = sorted(set().union(*df[column].apply(merge_lists)))
-    return result
