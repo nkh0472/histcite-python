035e1ff46eb113bcdb53bf42a7587dca46658707
WangK2 <kw221225@gmail.com>
2024-03-03 13:16:35 +0800

    Rewrite graph module

--- histcite/network_graph.py
+++ histcite/network_graph.py
@@ -21,29 +21,31 @@ class GraphViz:
             citation_relation: DataFrame of citation relationship.
             source: Data source. `wos`, `cssci` or `scopus`.
         """
-        self.empty_year_index: pd.Index = docs_df[docs_df["PY"].isna()].index
-        self.merged_docs_df: pd.DataFrame = docs_df.merge(
-            citation_relation,
-            left_index=True,
-            right_index=True,
-            suffixes=(None, "_y"),
-        ).drop(columns=["doc_id_y"])
-        self.source: Literal["wos", "cssci", "scopus"] = source
+        self.merged_docs_df = (
+            docs_df[docs_df["PY"].notna()]
+            .merge(
+                citation_relation,
+                left_index=True,
+                right_index=True,
+                suffixes=(None, "_y"),
+            )
+            .drop(columns=["doc_id_y"])
+        )
+        self.source = source
 
-    @classmethod
+    @staticmethod
     def generate_edge(
-        cls,
         doc_id: int,
-        related_doc_id_list: Union[str, list[int]],
+        related_doc_id: Union[str, list[int]],
         edge_type: Literal["cited", "citing"],
     ) -> set[tuple[int, int]]:
-        if isinstance(related_doc_id_list, str):
-            related_doc_id_list = [int(i) for i in related_doc_id_list.split("; ")]
+        if isinstance(related_doc_id, str):
+            related_doc_id = [int(i) for i in related_doc_id.split("; ")]
 
         if edge_type == "cited":
-            return {(doc_id, ref) for ref in related_doc_id_list}
+            return {(doc_id, ref) for ref in related_doc_id}
         elif edge_type == "citing":
-            return {(citation, doc_id) for citation in related_doc_id_list}
+            return {(citation, doc_id) for citation in related_doc_id}
 
     def from_specific_doc(
         self,
@@ -79,54 +81,53 @@ class GraphViz:
                 edge_set.update(self.generate_edge(idx, cited_doc_id, "cited"))
             if isinstance(citing_doc_id, str):
                 edge_set.update(self.generate_edge(idx, citing_doc_id, "citing"))
+        # Filter nodes not in the doc_id_list
         edge_set = {(edge[0], edge[1]) for edge in edge_set if edge[0] in doc_id_list and edge[1] in doc_id_list}
         return edge_set
 
-    def generate_edge_set(self) -> dict[int, list[int]]:
-        if len(self.doc_id_object) > 1:
-            edge_set = self.from_multi_doc(self.doc_id_object)
-        else:
-            initial_doc_id = self.doc_id_object[0]
-
-            if self.edge_type == "cited":
-                edge_set = self.from_specific_doc(initial_doc_id, "cited")
-            elif self.edge_type == "citing":
-                edge_set = self.from_specific_doc(initial_doc_id, "citing")
-            elif self.edge_type is None:
-                edge_set = self.from_specific_doc(initial_doc_id, "cited")
-                edge_set.update(self.from_specific_doc(initial_doc_id, "citing"))
+    def generate_edge_set(
+        self, doc_id_object: list[int], edge_type: Optional[Literal["cited", "citing"]]
+    ) -> set[tuple[int, int]]:
+        if len(doc_id_object) > 1:
+            edge_set = self.from_multi_doc(doc_id_object)
+        elif len(doc_id_object) == 1:
+            doc_id = doc_id_object[0]
+            if edge_type == "cited":
+                edge_set = self.from_specific_doc(doc_id, "cited")
+            elif edge_type == "citing":
+                edge_set = self.from_specific_doc(doc_id, "citing")
+            elif edge_type is None:
+                edge_set = self.from_specific_doc(doc_id, "cited")
+                edge_set.update(self.from_specific_doc(doc_id, "citing"))
             else:
-                raise ValueError('Argument <edge_type> must be one of "cited", "citing" or None')
+                raise ValueError('Argument <edge_type> should be one of "cited", "citing" or None')
+        else:
+            raise ValueError
+        return edge_set
 
-        # Drop node without PY info
-        if len(self.empty_year_index) > 0 and self.show_timeline is True:
-            edge_set = {
-                (edge[0], edge[1])
-                for edge in edge_set
-                if edge[0] not in self.empty_year_index and edge[1] not in self.empty_year_index
-            }
-
-        # Build node_list according to edges
-        source_node = set([i for i, _ in edge_set])
-        target_node = set([j for _, j in edge_set])
+    def set_node_list(self, edge_set: set[tuple[int, int]]) -> None:
+        source_node = set(i[0] for i in edge_set)
+        target_node = set(i[1] for i in edge_set)
         node_list = sorted(source_node | target_node)
         self.node_list = node_list
 
-        edge_dict: dict[int, list[int]] = {i: [] for i in sorted(source_node)}
+    def edge_set_to_dict(self, edge_set: set[tuple[int, int]]) -> dict[int, list[int]]:
+        source_node = set(i[0] for i in edge_set)
+        edge_dict = {node: [] for node in sorted(source_node)}
         for edge in edge_set:
             edge_dict[edge[0]].append(edge[1])
         return edge_dict
 
-    def obtain_groups(self) -> tuple[list[list[Hashable]], list[Hashable]]:
+    @staticmethod
+    def group_by_year(year_series: pd.Series, show_timeline: bool) -> tuple[list[Hashable], list[list[Hashable]]]:
         """Obtain groups of doc_id by year."""
-        year_series = self.merged_docs_df.loc[self.node_list, "PY"]
-        year_groups = year_series.groupby(year_series).groups.items()
-        year_list = [i[0] for i in year_groups]
-        grouped_doc_id = [list(i[1]) for i in year_groups]
-        if self.show_timeline is True:
+        year_groups = year_series.groupby(year_series).groups
+        year_list = list(year_groups.keys())
+        doc_id_group = [list(i) for i in year_groups.values()]
+        if show_timeline is True:
             for idx, year in enumerate(year_list):
-                grouped_doc_id[idx].insert(0, year)
-        return grouped_doc_id, year_list
+                doc_id_group[idx].insert(0, year)
+        return year_list, doc_id_group
 
     def generate_dot_file(
         self,
@@ -138,33 +139,31 @@ class GraphViz:
         Args:
             doc_id_object: Specific doc_id or list of doc_id. If list, only show edges between these doc_id.
             edge_type: Only for specific doc_id. It can be `cited`, `citing` or `None`. If `None`, show both `cited` and `citing` edges. Default None.
-            show_timeline: Whether show timeline. In some cases, timeline may be disorderly, so you can set it to `False`. Default True.
+            show_timeline: Whether show timeline. Default True.
 
         Returns:
             Dot file content.
         """
         if isinstance(doc_id_object, list) and len(doc_id_object) > 1:
             assert edge_type is None, "Argument <edge_type> should be None if <doc_id_object> contains >1 elements."
-            self.doc_id_object = doc_id_object
         elif isinstance(doc_id_object, int):
-            assert doc_id_object in self.merged_docs_df.index, "Don't specify <doc_id> not in <docs_df>."
-            assert doc_id_object not in self.empty_year_index, "Don't specify <doc_id> without <PY> info."
-            self.doc_id_object = [doc_id_object]
-        self.edge_type = edge_type
-        self.show_timeline = show_timeline
+            assert doc_id_object in self.merged_docs_df.index, "Please specify valid <doc_id_object>."
+            doc_id_object = [doc_id_object]
 
-        edge_dict = self.generate_edge_set()
-        grouped_doc_id, year_list = self.obtain_groups()
+        edge_set = self.generate_edge_set(doc_id_object, edge_type)
+        edge_dict = self.edge_set_to_dict(edge_set)
+        self.set_node_list(edge_set)
 
-        dot_groups = [
-            f'\t{{rank=same; {" ".join([str(i) for i in group_index])}}};\n' for group_index in grouped_doc_id
-        ]
+        year_series = self.merged_docs_df.loc[self.node_list, "PY"]
+        year_list, doc_id_group = self.group_by_year(year_series, show_timeline)
+
+        dot_groups = [f'\t{{rank=same; {" ".join([str(i) for i in group_index])}}};\n' for group_index in doc_id_group]
         dot_edge_list = [
             f"\t{source} -> " + "{ " + " ".join([str(i) for i in edge_dict[source]]) + " };\n"
             for source in edge_dict.keys()
         ]
 
-        if self.show_timeline is True:
+        if show_timeline is True:
             reversed_year_list = year_list[::-1]
             year_edge_list = [
                 (year, reversed_year_list[idx + 1])
